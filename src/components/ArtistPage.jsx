804e3125110037aca5aa4ba00366d487433b7214
Nestor162 <nestorcicardini@gmail.com>
2023-04-14 12:54:56 +0200

    Importati componenti bootstrap

--- src/components/ArtistPage.jsx
+++ src/components/ArtistPage.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 function ArtistPage() {
   return (
-    <div className="col-12 col-md-9 offset-md-3 mainPage">
-      <div className="row mb-3">
-        <div className="col-9 col-lg-11 mainLinks d-none d-md-flex">
+    <Col xs={12} md={9} className="offset-md-3 mainPage">
+      <Row className="mb-3">
+        <Col xs={9} lg={11} className="mainLinks d-none d-md-flex">
           <a href="#trending">TRENDING</a>
           <a href="#podcast">PODCAST</a>
           <a href="#moods">MOODS AND GENRES</a>
           <a href="#new">NEW RELEASES</a>
           <a href="#discover">DISCOVER</a>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12 col-md-10 col-lg-10 mt-5">
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={10} lg={10} className="mt-5">
           <h2 className="titleMain">&nbsp;</h2>
           <div id="followers"></div>
           <div className="d-flex justify-content-center" id="button-container">
@@ -25,19 +25,19 @@ function ArtistPage() {
               FOLLOW
             </Button>
           </div>
-        </div>
-      </div>
-      <div className="row mb-3">
-        <div className="col-10 offset-1 col-md-10 col-lg-10 p-0">
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col xs={10} md={10} lg={10} className="offset-1 p-0">
           <div className="mt-4 d-flex justify-content-start">
             <h2 className="text-white font-weight-bold">Tracks</h2>
           </div>
           <div className="pt-5 mb-5">
-            <div className="row" id="apiLoaded" />
+            <Row className="row" id="apiLoaded" />
           </div>
-        </div>
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Col>
   );
 }
 
