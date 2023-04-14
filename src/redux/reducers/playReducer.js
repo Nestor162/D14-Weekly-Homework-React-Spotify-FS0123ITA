caeb11392ac24571f89c30738c0b9365062f0e98
Nestor162 <nestorcicardini@gmail.com>
2023-04-14 17:54:11 +0200

    Aggiunto Commento

--- src/redux/reducers/playReducer.js
+++ src/redux/reducers/playReducer.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// Seguendo anche la documentazione, ho deciso di usare gli Slice perche facilitano molto la creazione di Reducers e la gestione dello stato.
+
 export const playReducer = createSlice({
   name: "player",
   initialState: {
