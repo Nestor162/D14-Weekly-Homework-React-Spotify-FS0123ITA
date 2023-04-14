40e537288647e68666601adcd7b41459a901f3df
Nestor162 <nestorcicardini@gmail.com>
2023-04-14 16:48:31 +0200

    Eliminato import non necessario

--- src/components/NavbarBottom.jsx
+++ src/components/NavbarBottom.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "./NavbarBottom.css";
 import Play from "../img/playerbuttons/Play.png";
