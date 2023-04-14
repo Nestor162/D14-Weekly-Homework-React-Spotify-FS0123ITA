df15c4419858a65fc27ed0915584b66928bd4a97
Nestor162 <nestorcicardini@gmail.com>
2023-04-14 12:58:35 +0200

    Creato sistema di routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AlbumPage from "./components/AlbumPage.jsx";
+import ArtistPage from "./components/ArtistPage";
+import Homepage from "./components/Homepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/artist:artistId" element={<ArtistPage />} />
+          <Route path="/album:albumId" element={<AlbumPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
