2cf9352bfc867b2368365086ddc107a4be977908
Nestor162 <nestorcicardini@gmail.com>
2023-04-15 12:23:13 +0200

    Migliorata responsiveness

--- src/components/ArtistPage.jsx
+++ src/components/ArtistPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 function ArtistPage() {
   const params = useParams();
@@ -48,68 +49,72 @@ function ArtistPage() {
   }, [artist]);
 
   return (
-    <Col xs={12} md={9} className="offset-md-3 mainPage">
-      <Row className="mb-3">
-        <Col xs={9} lg={11} className="mainLinks d-none d-md-flex">
-          <a href="#trending">TRENDING</a>
-          <a href="#podcast">PODCAST</a>
-          <a href="#moods">MOODS AND GENRES</a>
-          <a href="#new">NEW RELEASES</a>
-          <a href="#discover">DISCOVER</a>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} md={10} lg={10} className="mt-5">
-          <div className="d-flex flex-column justify-content-center" id="button-container">
-            {artist && (
-              <>
-                <img className="mx-auto mb-3" src={artist.picture_big} alt={artist.name}></img>
-                <h2 class="titleMain">{artist.name}</h2>
-                <div id="followers">{artist.nb_fan} followers</div>
-              </>
-            )}
+    <>
+      <Sidebar />
+      <Col xs={12} md={9} className="offset-md-3 mainPage">
+        <Row className="mb-3">
+          <Col xs={9} lg={11} className="mainLinks d-none d-md-flex">
+            <a href="#trending">TRENDING</a>
+            <a href="#podcast">PODCAST</a>
+            <a href="#moods">MOODS AND GENRES</a>
+            <a href="#new">NEW RELEASES</a>
+            <a href="#discover">DISCOVER</a>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={10} lg={10} className="mt-5">
+            <div className="d-flex flex-column justify-content-center" id="button-container">
+              {artist && (
+                <>
+                  <img className="mx-auto mb-3" src={artist.picture_big} alt={artist.name}></img>
+                  <h2 class="titleMain">{artist.name}</h2>
+                  <div id="followers">{artist.nb_fan} followers</div>
+                </>
+              )}
 
-            <div className="buttonsWrappers d-flex justify-content-center">
-              <Button className="btn-success me-2 mainButton" id="playButton">
-                PLAY
-              </Button>
-              <Button variant="outline-light" className="mainButton" id="followButton">
-                FOLLOW
-              </Button>
+              <div className="buttonsWrappers d-flex justify-content-center">
+                <Button className="btn-success me-2 mainButton" id="playButton">
+                  PLAY
+                </Button>
+                <Button variant="outline-light" className="mainButton" id="followButton">
+                  FOLLOW
+                </Button>
+              </div>
             </div>
-          </div>
-        </Col>
-      </Row>
-      <Row className="mb-3">
-        <Col xs={10} md={10} lg={10} className="offset-1 p-0">
-          <div className="mt-4 d-flex justify-content-start">
-            <h2 className="text-white font-weight-bold">Tracks</h2>
-          </div>
-          <div className="pt-5 mb-5">
-            <Row className="row" id="apiLoaded" />
-            {tracklist && (
-              <>
-                {tracklist.data.map(song => {
-                  return (
-                    <>
-                      <div class="col-sm-auto col-md-auto text-center mb-5">
-                        <a href="/album_page.html?id=340878">
-                          <img class="img-fluid" src={song.album.cover_big} alt="1" />
-                        </a>
-                        <p>
-                          <a href="#link">Track: {song.title}"</a>
-                          <a href="/album_page.html?id=340878">Album: {song.album.title}</a>
-                        </p>
-                      </div>
-                    </>
-                  );
-                })}
-              </>
-            )}
-          </div>
-        </Col>
-      </Row>
-    </Col>
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Col xs={10} md={10} lg={10} className="offset-1 p-0">
+            <div className="mt-4 d-flex justify-content-start">
+              <h2 className="text-white font-weight-bold">Tracks</h2>
+            </div>
+            <div className="pt-5 mb-5">
+              <Row id="apiLoaded">
+                {tracklist && (
+                  <>
+                    {tracklist.data.map(song => {
+                      return (
+                        <>
+                          <Col xs={12} sm={6} md={4} className="text-center mb-5">
+                            <a href="/album_page.html?id=340878">
+                              <img class="img-fluid" src={song.album.cover_big} alt="1" />
+                            </a>
+                            <p>
+                              <a href="#link">Track: {song.title}"</a>
+                              <a href="/album_page.html?id=340878">Album: {song.album.title}</a>
+                            </p>
+                          </Col>
+                        </>
+                      );
+                    })}
+                  </>
+                )}
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </>
   );
 }
 
